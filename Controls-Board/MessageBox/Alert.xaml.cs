a2747f18fec0b6858a39c85352884f4f1516ebb6
SnowyPainter <SnowyPainter@gmail.com>
2020-12-25 09:26:00 +0900

    Add messagebox to replace window.msgbox

---
+++ Controls-Board/MessageBox/Alert.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Controls_Board.MessageBox
+{
+    /// <summary>
+    /// Alert.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Alert : Window
+    {
+        public Alert(string msg)
+        {
+            InitializeComponent();
+            Descript.Text = msg;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Controls-Board/MessageBox/OkMessageBox.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Controls_Board.MessageBox
+{
+    /// <summary>
+    /// OkMessageBox.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class OkMessageBox : Window
+    {
+        bool ok = false;
+        public OkMessageBox(string description)
+        {
+            InitializeComponent();
+            Descript.Text = description;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            ok = false;
+            this.Close();
+        }
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            ok = true;
+            this.Close();
+        }
+
+        public bool Ok()
+        {
+            return ok;
+        }
+        
+    }
+}
